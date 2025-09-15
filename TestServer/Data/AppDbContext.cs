1a23433b42e2e1b48df487b5e9a2a535f83602bd
Nht <hungthai1804@gmail.com>
2025-09-15 21:09:30 +0700

    14th test

--- TestServer/Data/AppDbContext.cs
+++ TestServer/Data/AppDbContext.cs
@@ -9,18 +9,20 @@ public class AppDbContext : DbContext
     {
     }
 
-    public DbSet<Driver> Drivers { get; set; }
-
     // Thêm đoạn code này để seeding data
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         base.OnModelCreating(modelBuilder);
 
         modelBuilder.Entity<Driver>().HasData(
-            new Driver { Id = 1, Name = "Lewis Hamilton" },
-            new Driver { Id = 2, Name = "Max Verstappen" },
-            new Driver { Id = 3, Name = "Charles Leclerc" },
-            new Driver { Id = 4, Name = "Sergio Pérez" } // Thêm một vài driver nữa
+            new Driver { Id = 1, Name = "Nguyễn Xuân Thịnh" },
+            new Driver { Id = 2, Name = "Nguyễn Hưng Thái" },
+            new Driver { Id = 3, Name = "Nguyễn Bùi Đăng Khôi" },
+            new Driver { Id = 4, Name = "Lê Minh Đức" } ,
+            new Driver { Id = 5, Name = "Vũ Thế Anh" }
         );
     }
+
+    public DbSet<Driver> Drivers { get; set; } = null!;
+
 }
--- TestServer/obj/Debug/net9.0/TestServer.AssemblyInfo.cs
+++ TestServer/obj/Debug/net9.0/TestServer.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestServer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+acee998c1a0e5f56ec1763548984302778c94128")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8731d8ce00d501f5713470d88334b09c57610306")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestServer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestServer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
