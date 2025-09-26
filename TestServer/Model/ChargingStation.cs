845a35b949180bbfe6ddb6f5671ac6685af156f1
Kxt2412 <nguyenbuidangkhoixt@gmail.com>
2025-09-26 14:00:42 +0700

    update 10

--- TestServer/Model/ChargingStation.cs
+++ TestServer/Model/ChargingStation.cs
@@ -5,8 +5,13 @@ using System.ComponentModel.DataAnnotations.Schema;
 namespace TestServer.Models;
 public class ChargingStation
     {
+        [Key]
         public int Id { get; set; }
+
+        [Required]
         public string Name { get; set; } = string.Empty;
+
+        [Required]
         public string Location { get; set; } = string.Empty;
         
     }
