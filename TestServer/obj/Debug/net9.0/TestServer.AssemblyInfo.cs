9bbd7add6002908940862912cf6e0d8a1fbe64f6
Nguyen Bui Đang Khoi <nguyenbuidangkhoipnn@gmail.com>
2025-10-09 23:15:57 +0700

    update 1

--- TestServer/obj/Debug/net9.0/TestServer.AssemblyInfo.cs
+++ TestServer/obj/Debug/net9.0/TestServer.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestServer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+68c307e69b361839a18cc9bb438fdc53d3cc5380")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c32aa4b51c427d24d8ca423b751553a5600c1e8e")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestServer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestServer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
