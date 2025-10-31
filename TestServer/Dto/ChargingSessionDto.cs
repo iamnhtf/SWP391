76d8d9675212551b99a122870987d1e910a54a2f
Nguyễn Xuân Thịnh <xuanthinhkl1@gmail.com>
2025-10-31 12:42:27 +0700

    Change StartTime and EndTime to string in DTO
    Updated ChargingSessionDto to use string types for StartTime and EndTime instead of DateTime. Adjusted ChargingSessionController to convert DateTime values to string when populating DTOs, improving serialization and compatibility with clients.

--- TestServer/Dto/ChargingSessionDto.cs
+++ TestServer/Dto/ChargingSessionDto.cs
@@ -9,9 +9,9 @@ namespace TestServer.Dto
         public string CustomerId { get; set; } = string.Empty;
         public ChargingPortInfoDto PortInfo { get; set; } = new ChargingPortInfoDto();
         public string StartTimeStr { get; set; } = string.Empty;
-        public DateTime StartTime { get; set; }
+        public string StartTime { get; set; } = string.Empty;
         public string EndTimeStr { get; set; } = string.Empty;
-        public DateTime EndTime { get; set; }
+        public string EndTime { get; set; } = string.Empty;
         public string Duration { get; set; } = string.Empty;
         public float EnergyConsumed { get; set; }
         public float TotalCost { get; set; }
--- TestServer/obj/Debug/net9.0/TestServer.AssemblyInfo.cs
+++ TestServer/obj/Debug/net9.0/TestServer.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestServer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ddba5849bd5b576daccb8e23532a052212ac4d4b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bb3dd3e5f418ac659da7de2ca1607275f42bdf11")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestServer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestServer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
