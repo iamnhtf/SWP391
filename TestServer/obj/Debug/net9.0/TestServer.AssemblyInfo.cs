32cb2d2f50afd3e72d7eee4c53aa95eb82f8a6f7
iamnhtf <hungthai1804@gmail.com>
2025-11-13 14:14:41 +0700

    update PaymentTransaction

--- TestServer/obj/Debug/net9.0/TestServer.AssemblyInfo.cs
+++ TestServer/obj/Debug/net9.0/TestServer.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestServer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cd4d7293cb38088ef730a7b4986d1ba99c1117f1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c732442ed0ff36a092b73b8122bdd830d5bf2f19")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestServer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestServer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
